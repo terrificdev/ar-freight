df2064c3858cc2b696f5beffd10d651e97826607
sangeetha-dev <sangeetha@terrificminds.com>
2019-02-26 19:42:39 +0530

    conflict resolved

--- wp-content/themes/ar-freight/js/custom.js
+++ wp-content/themes/ar-freight/js/custom.js
@@ -60,5 +60,30 @@ jQuery(document).ready(function ($) {
       }
     ]
   });
-
+  $(".center_slick").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true
+    }
+  },
+  {
+    breakpoint: 450,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true
+    }
+  },
+]
+});
 });
