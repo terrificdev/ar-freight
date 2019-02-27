70c8469d5c93a0c83e5ae52d5caf4fb265bb5780
dyodedev <nithin@terrificminds.com>
2019-02-27 22:52:20 +0530

    ARF-35 #comment social media icons in banner

--- wp-content/themes/ar-freight/template-parts/content-banner.php
+++ wp-content/themes/ar-freight/template-parts/content-banner.php
@@ -43,5 +43,20 @@
             </div>
             <?php endif;?>
         </div>
+        <div class = "banner-social-block">
+            <?php if ( has_nav_menu( 'social' ) ) : ?>
+                <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'ar-freight' ); ?>">
+                    <?php
+                        wp_nav_menu( array(
+                            'theme_location' => 'social',
+                            'menu_class'     => 'social-links-menu',
+                            'depth'          => 1,
+                            'link_before'    => '<span class="screen-reader-text">',
+                            'link_after'     => '</span>',
+                        ) );
+                    ?>
+                </nav><!-- .social-navigation -->
+            <?php endif; ?>
+        </div>
     </div>
 </div>
