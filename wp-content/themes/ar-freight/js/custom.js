fc8861c4c85ad97de4dfce366b16a38f59838bfa
sangeetha-dev <sangeetha@terrificminds.com>
2019-02-28 16:23:16 +0530

    ARF-152 #comment UI task for mini about and boxed section - Home page #commit by sangeetha

--- wp-content/themes/ar-freight/js/custom.js
+++ wp-content/themes/ar-freight/js/custom.js
@@ -87,4 +87,26 @@ jQuery(document).ready(function ($) {
   },
 ]
 });
+ //Hover interaction for news item
+ var width = $(window).width();
+ if (width > 1025) {
+   //desktop interaction
+   var toggleOn = false;
+   $(".about-us__right__block").hover(
+     function () {
+       $(this).addClass('active');
+     }, function () {
+       toggleOn || $(this).removeClass('active');
+     }
+   );
+ }
+ else {
+   // tablet and mobile interaction
+   $(".about-us__right__block").click(function () {
+     $(".about-us__right__block").removeClass('active');
+     $(this).toggleClass('active');
+
+   });
+ }
+ 
 });
