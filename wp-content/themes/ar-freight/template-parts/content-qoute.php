fd87cb1ee75a50b72d929566b04de3324b18552e
Nisful Laila M. I <nisful@terrificminds.com>
2019-03-13 21:33:10 +0530

    short code for form updated

--- wp-content/themes/ar-freight/template-parts/content-qoute.php
+++ wp-content/themes/ar-freight/template-parts/content-qoute.php
@@ -1,22 +1,3 @@
-<!-- <div class = "get-a-quote-container">
-    <div class = "get-a-quote-wrapper">
-        <div class = "quote-left">
-            <h2>Get a quote</h2>
-            <p>Select the main category below and fill in the details</p>
-            <button class = "type-of-services" data-name="Relocation Services">Relocation Services</button>
-            <button class = "type-of-services" data-name="Freight Services">Freight Services</button>
-            <button class = "type-of-services" data-name="Other Services">Other Services</button>
-        </div>
-        <div class = "quote-right">
-            <div id="quote-title">
-                <h2>Relocation Services</h2>
-            </div>
-            <div class = "quote-form">
-                <?php echo do_shortcode('[contact-form-7 id="176" title="Get A Qoute"]');?>
-            </div>
-        </div>
-    </div>
-</div> -->
 <div id="quoteModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
         <!-- Modal content-->
@@ -39,7 +20,7 @@
                                 <h2>Relocation Services</h2>
                             </div>
                             <div class = "quote-form">
-                                <?php echo do_shortcode('[contact-form-7 id="180" title="contact-form-7"]');?>
+                                <?php echo do_shortcode('[contact-form-7 id="219" title="Quote form"]');?>
                             </div>
                         </div>
                     </div>
