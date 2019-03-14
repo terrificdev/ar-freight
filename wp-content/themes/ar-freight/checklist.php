a19521a07ec3b04a5ed00c93db58714da97a4158
jeevan-gopakumar <jeevan.gopakumar@dyode.com>
2019-03-14 10:17:46 +0530

    #ARF-188 - UI task for Moving checklists page with accordion menu - Commit by Jeevan

--- wp-content/themes/ar-freight/checklist.php
+++ wp-content/themes/ar-freight/checklist.php
@@ -8,8 +8,8 @@
         <?php while ( have_posts() ) : the_post(); ?>
         <?php
         $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );?>
-            <div class = "checklist-container">
-                <div class = "checklist-wrapper">
+            <div class = "other-page-wrapper checklist-wrapper">
+                <div class = "other-page-container checklist-container">
                     <div class = "checklist-banner">
                         <div class = "checklist-banner__image">
                             <img src ="<?php echo wp_get_attachment_url(get_theme_mod('checklist_banner'))?>">
@@ -29,29 +29,39 @@
                          </div>
                     </div>
                     <div class = "checklist-listing-wrapper">
-                        <div class = "checklist-listing">
-                        <?php 
-                        $checklist = new WP_Query(array(
-                            'post_type' => 'checklist',
-                            'post_status' => 'publish',
-                            'posts_per_page' => -1,
-                            'order' => 'ASC',
-                        ));?>
-                        <?php while ($checklist->have_posts()) :
-                            $checklist->the_post();
-                            $post_id = get_the_ID();?>
-                                <div class = "checklist-block">
-                                    <div class = 'checklist-title'>
-                                        <h2><?php echo get_the_title();?></h2>
-                                    </div>
-                                    <div class = 'checklist-content'>
-                                        <?php the_content()?>    
-                                    </div>
-                                </div>
-                            <?php
-                            endwhile;
-                            wp_reset_query(); ?>                   
-                        </div>
+                      <div class = "checklist-listing  block-container">
+													<div class="faq-panel-group" id="accordion">
+														<?php
+														$count=1;
+														$checklist = new WP_Query(array(
+																'post_type' => 'checklist',
+																'post_status' => 'publish',
+																'posts_per_page' => -1,
+																'order' => 'ASC',
+														));?>
+														<?php while ($checklist->have_posts()) :
+														$checklist->the_post();
+														$post_id = get_the_ID();?>
+
+														<div class="panel checklist-panel panel-default">
+															<div class="panel-heading">
+													      <h4 class="panel-title title">
+													        <a data-toggle="collapse" data-parent="#accordion" class="open collapsed" href="#faq<?php echo $count; ?>">
+													        <?php echo get_the_title();?></a>
+													      </h4>
+													    </div>
+															<div id="faq<?php echo $count; ?>" class="panel-collapse collapse">
+													      <div class="panel-body">
+																	<?php the_content()?>
+																</div>
+													    </div>
+														</div>
+														<?php
+														$count++;
+														endwhile;
+														wp_reset_query(); ?>
+		                      </div>
+											</div>
                     </div>
                 </div>
             </div>
