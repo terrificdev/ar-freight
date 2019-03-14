01556cedad33a87b166b9eabc8b71a04f1a724a6
nithinterrific <nithin@terrificminds.com>
2019-03-14 19:53:01 +0530

    Fixed the search issue

--- wp-content/themes/ar-freight/news_events.php
+++ wp-content/themes/ar-freight/news_events.php
@@ -21,9 +21,8 @@
 													<?php echo get_the_title();?>
 											</div>
 											<div class = "news-events-banner__searchBox">
-												<form role="search" action="<?php the_permalink(); ?>" method="get" id="searchform">
-													<input type="text" name="search" class="search-text" placeholder="Search for news and events" value="<?php $_REQUEST['search']?>"/>
-													<input type="hidden" name="post_type" value="news" /> <!-- // hidden 'news' value -->
+												<form role="search" action="<?php the_permalink(); ?>" method="GET" id="searchform">
+													<input type="text" name="search" class="search-text" placeholder="Search for news and events" value="<?php echo $_REQUEST['search']?>"/>
 													<input type="submit" alt="Search" class="searchBtn" value="" />
 												</form>
 											</div>
@@ -33,31 +32,32 @@
 					</div>
 
                     <div class="block-container">
-                        <?php if( isset( $_REQUEST['search'] ) ):?>
-                            <h2>Search Result For "<?php echo $_REQUEST['search']?>"</h2>
-                        <?php endif;?>    
+                        <?php if( isset($_REQUEST['search']) && $_REQUEST['search'] != "" ):?>
+                            <h2 class = "search-status">Search Result For "<?php echo $_REQUEST['search']?>"</h2>
+                        <?php endif;?>  
+                        <?php
+                        $news = new WP_Query(array(
+                            'post_type' => 'news',
+                            'post_status' => 'publish',
+                            'posts_per_page' => -1,
+                            's' =>  $_REQUEST['search'],   
+                            'tax_query' => array(
+                                array (
+                                    'taxonomy' => 'news_category',
+                                    'field' => 'slug',
+                                    'terms' => 'news',
+                                )
+                            ),
+                        ));
+                        if ($news->post_count > 0): 
+                        while ($news->have_posts()):
+                            $news->the_post();
+                            $post_id = get_the_ID();
+                            $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
+                        ?>  
                         <div class = "news-content">
                             <div class="news-slider news-list-slider">
                             <section class="regular slider loadMore">
-                                <?php
-                                $news = new WP_Query(array(
-                                    'post_type' => 'news',
-                                    'post_status' => 'publish',
-                                    'posts_per_page' => -1,
-                                    's' =>  $_REQUEST['search'],   
-                                    'tax_query' => array(
-                                        array (
-                                            'taxonomy' => 'news_category',
-                                            'field' => 'slug',
-                                            'terms' => 'news',
-                                        )
-                                    ),
-                                ));
-                                while ($news->have_posts()):
-                                    $news->the_post();
-                                    $post_id = get_the_ID();
-                                    $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
-                                ?>
                                 <div class="news-list">
                                     <div class = "news-block content-block">
                                         <div class = "news-image slider-image">
@@ -76,58 +76,61 @@
                                         </div>
                                     </div>
                                 </div>
-                                <?php
-                                endwhile;
-                                wp_reset_query(); ?>
                             </section>
                             </div>
                         </div>
+                        <?php
+                        endwhile;
+                        endif;
+                        wp_reset_query(); ?>
+                        <?php
+                        $events = new WP_Query(array(
+                            'post_type' => 'news',
+                            'post_status' => 'publish',
+                            'posts_per_page' => -1,
+                            's' =>  $_REQUEST['search'], 
+                            'tax_query' => array(
+                                array (
+                                    'taxonomy' => 'news_category',
+                                    'field' => 'slug',
+                                    'terms' => 'events',
+                                )
+                            ),
+                        ));
+                        if ($events->post_count > 0):
+                        while ($events->have_posts()):
+                            $events->the_post();
+                            $post_id = get_the_ID();
+                            $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
+                        ?>
                         <div class = "events-content">
                             <div class="event-slider events-list-slider">
-                            <section class="regular slider loadMore">
-                            <?php
-                            $events = new WP_Query(array(
-                                'post_type' => 'news',
-                                'post_status' => 'publish',
-                                'posts_per_page' => -1,
-                                's' =>  $_REQUEST['search'], 
-                                'tax_query' => array(
-                                    array (
-                                        'taxonomy' => 'news_category',
-                                        'field' => 'slug',
-                                        'terms' => 'events',
-                                    )
-                                ),
-                            ));
-                            while ($events->have_posts()):
-                                $events->the_post();
-                                $post_id = get_the_ID();
-                                $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
-                            ?>
-                            <div class="news-list">
-                                <div class = "events-block content-block">
-                                    <div class = "events-image slider-image">
-                                        <img class = "events-featured-image" src="<?php echo $featuredImage[0]; ?>">
-                                    </div>
-                                    <div class="events-details-wrapper slider-details">
-                                        <div class = "events-title content-title">
-                                            <p><?php echo get_the_title();?></p>
-                                        </div>
-                                        <div class = "events-excerpt content-excerpt">
-                                            <?php the_excerpt();?>
-                                        </div>
-                                        <div class = "events-readmore content-readmore">
-                                        <a href="<?php echo get_permalink(get_option('page_for_posts'));?>">Read More</a>
+                                <section class="regular slider loadMore">
+                                    <div class="news-list">
+                                        <div class = "events-block content-block">
+                                            <div class = "events-image slider-image">
+                                                <img class = "events-featured-image" src="<?php echo $featuredImage[0]; ?>">
+                                            </div>
+                                            <div class="events-details-wrapper slider-details">
+                                                <div class = "events-title content-title">
+                                                    <p><?php echo get_the_title();?></p>
+                                                </div>
+                                                <div class = "events-excerpt content-excerpt">
+                                                    <?php the_excerpt();?>
+                                                </div>
+                                                <div class = "events-readmore content-readmore">
+                                                <a href="<?php echo get_permalink(get_option('page_for_posts'));?>">Read More</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                </section>
                             </div>
-                            <?php
-                            endwhile;
-                            wp_reset_query(); ?>
-                            </section>
                         </div>
-                        </div>
+                        <?php
+                        endwhile;
+                        endif;
+                        wp_reset_query(); ?>
                     </div>
 				</div>
 			</div>
@@ -135,5 +138,12 @@
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </div><!-- .wrap -->
+<?php if($events->post_count == 0 && $news->post_count == 0):?>
+<script>
+$(document).ready(function(){
+    $(".search-status").html("No Search Result Found!");
+});
+</script>
+<?php endif;?>
 
 <?php get_footer();
